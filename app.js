ab78835cab8205d18b33fd35a2bdca1cb5247788
David Hayes <davidlynnhayes@att.net>
2015-08-25 13:03:30 -0500

    db sms

--- app.js
+++ app.js
@@ -17,10 +17,11 @@ sms.modelView = Backbone.View.extend({
   el: $('.list'),
   initialize: function() {
     console.log('A modelView has been instantiated.');
+    this.render();
   },
   render: function() {
-    var data;
-    var tpl = 'Sent from: ' + data.sender + ' >> ' + data.message;
+    var data = this.model.attributes;
+    var tpl = '<li>Sent from: ' + data.sender + ' >> ' + data.message + '</li>';
     this.$el.append(tpl);
   }
 });
@@ -30,9 +31,22 @@ sms.collectionView = Backbone.View.extend({
   },
   initialize: function() {
     console.log('A collectionView has been instantiated.');
+    // this collection.on('sync') -- live with a server
+    var that = this;
+    this.collection.on('change', function() {
+      // this refers to the collection (not the view)
+      that.render();
+    });
   },
   render: function() {
-    console.log('clicked!');
+    var collection = this.collection.models; //[] where the models are
+    // loop through all models in our collection
+    for (var model in collection) {
+      new sms.modelView({
+        model: collection[model]
+      });
+    };
+    console.log('you clicked!');
   }
 });
 // end blueprints
@@ -44,6 +58,15 @@ $(document).ready(function() {
   active.collectionView = new sms.collectionView({
     el: $('.ui'),
     collection: active.collection
+  });
+  $('#new').on('click', function() {
+
+    //collection creates a model based on the model attribute
+    // inside of the collection blueprint/class/def etc
+    active.collection.create({
+      sender: 'Regina',
+      message: 'Do you need some money?'
+    });
   });
 });
 // end active
